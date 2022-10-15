80b85ad6d937ea89870f1a68afda89cc8825291b
JeongAYoo <jeonga038@naver.com>
2022-10-15 13:51:47 +0900

    [ADD] 9498

---
+++ 조건문/9498.swift
@@ -0,0 +1,19 @@
+/*
+ 9498 시험 성적
+ https://www.acmicpc.net/problem/9498
+ */
+
+let score = Int(readLine()!)!
+
+switch score {
+case 90...100:
+    print("A")
+case 80...89:
+    print("B")
+case 70...79:
+    print("C")
+case 60...69:
+    print("D")
+default:
+    print("F")
+}
