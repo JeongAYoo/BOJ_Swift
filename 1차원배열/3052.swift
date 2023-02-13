27df67933b3c85518088084b2a6d1b1d69c732c0
Jade Yoo <jeonga038@naver.com>
2023-02-13 23:41:13 +0900

    Update 3052.swift

--- 1차원배열/3052.swift
+++ 1차원배열/3052.swift
@@ -3,12 +3,10 @@
  https://www.acmicpc.net/problem/3052
  */
 
-var numbers: [Int] = []
 var remains = Set<Int>()
 
 for _ in 1...10 {
     let n = Int(readLine()!)!
-    numbers.append(n)
     remains.insert(n % 42)
 }
 
