f0278c7f0984738cded1f2e6edfd355c4e14bdac
JeongAYoo <jeonga038@naver.com>
2023-05-15 23:09:46 +0900

    [ADD] 9086

---
+++ 문자열/9086.swift
@@ -0,0 +1,11 @@
+/*
+ 9086 문자열
+ https://www.acmicpc.net/problem/9086
+ */
+
+let n = Int(readLine()!)!
+
+for _ in 0..<n {
+    let input = readLine()!
+    print(input.first!, input.last!, separator: "")
+}
